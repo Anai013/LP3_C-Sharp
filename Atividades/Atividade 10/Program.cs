62dff7dffa83b7abbc45b708e4179a11fbd01ca8
Anaí Villca Rojas <77812008+Anai013@users.noreply.github.com>
2021-09-02 17:45:22 -0300

    Atividade 10
    Exercício sobre Herança

---
+++ Atividades/Atividade 10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro cachorro = new Cachorro();
+            Gato gato = new Gato();
+            Jacare jacare = new Jacare();
+            Sapo sapo = new Sapo();
+        }
+    }
+}
