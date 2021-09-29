87a530082e1528cc542544326ab8309cff83a456
Anaí Villca Rojas <77812008+Anai013@users.noreply.github.com>
2021-09-29 16:27:32 -0300

    Atividade 12
    Exercícios sobre classes abstratas

---
+++ Atividades/Aula12/Aula12/Circulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12
+{
+    class Circulo : Forma
+    {
+        private double raio;
+
+        public Circulo(double raio)
+        {
+            Console.WriteLine("**********Circulo**********");
+            base.setCor("Roxo");
+            this.raio = raio;
+            Console.WriteLine("Area do 1° Circulo: " + this.calcularArea()+"\n");
+        }
+
+        public Circulo(double raio, string cor)
+        {
+            base.setCor(cor);
+            this.raio = raio;
+            Console.WriteLine("Area do 2° Circulo: " + this.calcularArea());
+        }
+
+        public override double calcularArea()
+        {
+            return 3.14 * (raio * raio);
+        }
+
+    }
+}
---
+++ Atividades/Aula12/Aula12/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12
+{
+    class Retangulo : Forma
+    {
+        private double altura;
+        private double largura;
+
+        public Retangulo(double altura, double largura)
+        {
+            Console.WriteLine("\n**********Retangulo**********");
+            base.setCor("Vermelho");
+            this.altura = altura;
+            this.largura = largura;
+            Console.WriteLine("Area do 1° Retangulo: " + this.calcularArea() + "\n");
+        }
+
+        public Retangulo(double altura, double largura, string cor)
+        {
+            base.setCor(cor);
+            this.altura = altura;
+            this.largura = largura;
+            Console.WriteLine("Area do 2° Retangulo: " + this.calcularArea());
+        }
+
+        public override double calcularArea()
+        {
+            return altura * largura;    
+        }
+    }
+}
