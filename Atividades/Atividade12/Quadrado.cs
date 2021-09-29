4697091e9adac166c81ef259f6bd8bd8724229ce
Anaí Villca Rojas <77812008+Anai013@users.noreply.github.com>
2021-09-29 16:33:15 -0300

    Atividade 12
    Exercícios sobre classes abstratas

---
+++ Atividades/Atividade12/Quadrado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12
+{
+    class Quadrado : Forma
+    {
+        private double lado;
+
+        public Quadrado(double lado)
+        {
+            Console.WriteLine("\n**********Quadrado**********");
+            base.setCor("Azul");
+            this.lado = lado;
+            Console.WriteLine("Area do 1° Quadrado: " + this.calcularArea() + "\n");
+        }
+
+        public Quadrado(double lado, string cor)
+        {
+            base.setCor(cor);
+            this.lado = lado;
+            Console.WriteLine("Area do 2° Quadrado: " + this.calcularArea());
+        }
+
+        public override double calcularArea()
+        {
+            return lado * lado;
+        }
+    }
+}
